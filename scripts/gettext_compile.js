9dcfef7dc674104490914131806b49fa720ddec1
Leo Zurbriggen <zurbriggen@joshmartin.ch>
2021-03-26 09:16:07 +0100

    fix script bug when using default locale

--- scripts/gettext_compile.js
+++ scripts/gettext_compile.js
@@ -9,12 +9,13 @@ let outDir = "./src/language";
 if (outIndex > -1) {
   outDir = process.argv[outIndex + 1];
 }
+
 const localesIndex = process.argv.indexOf("--locales");
 let locales = "en_US";
 if (localesIndex > -1) {
   locales = process.argv[localesIndex + 1];
-  locales = locales.split(",").map((l) => l.trim());
 }
+locales = locales.split(",").map((l) => l.trim());
 
 console.log(`Language directory: ${outDir}`);
 console.log(`Locales: ${locales}`);
@@ -22,7 +23,7 @@ console.log("");
 
 function execShellCommand(cmd) {
   const exec = require("child_process").exec;
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
         console.warn(error);
--- scripts/gettext_extract.js
+++ scripts/gettext_extract.js
@@ -20,8 +20,8 @@ const localesIndex = process.argv.indexOf("--locales");
 let locales = "en_US";
 if (localesIndex > -1) {
   locales = process.argv[localesIndex + 1];
-  locales = locales.split(",").map((l) => l.trim());
 }
+locales = locales.split(",").map((l) => l.trim());
 
 const potPath = `${outDir}/messages.pot`;
 console.log(`Source directory directory: ${srcDir}`);
@@ -31,7 +31,7 @@ console.log("");
 
 function execShellCommand(cmd) {
   const exec = require("child_process").exec;
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
         console.warn(error);
