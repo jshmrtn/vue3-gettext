76317428732d37870b299e0391e1d1937fb694b4
lzurbriggen <2656903+lzurbriggen@users.noreply.github.com>
2022-03-29 09:57:08 +0000

    Deploying to pages from @ jshmrtn/vue3-gettext@563cc828152e73ade51168d991d477dbcbc085b2 🚀

--- assets/404.html.54e38111.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app.8a0aa168.js";const _={};function e(t,c){return null}var a=r(_,[["render",e]]);export{a as default};
---
+++ assets/demo.html.2bdc27e0.js
@@ -0,0 +1,1 @@
+const e={key:"v-2b30cdfd",path:"/demo.html",title:"Demo",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1648547733e3,contributors:[{name:"Leo Zurbriggen",email:"zurbriggen@joshmartin.ch",commits:1}]},filePathRelative:"demo.md"};export{e as data};
