be13ffbd09cc7505a1e5122ef0376a01e9bec9fa
Leo Zurbriggen <zurbriggen@joshmartin.ch>
2022-03-29 11:49:20 +0200

    fix: #21 - properly serialize text nodes within template tags

--- scripts/embeddedJsExtractor.ts
+++ scripts/embeddedJsExtractor.ts
@@ -1,8 +1,33 @@
 import { Element, IHtmlExtractorFunction } from "gettext-extractor/dist/html/parser";
 import { ElementSelectorSet } from "gettext-extractor/dist/html/selector";
-import { HtmlUtils } from "gettext-extractor/dist/html/utils";
 import { JsParser } from "gettext-extractor/dist/js/parser";
+import { IContentOptions, normalizeContent } from "gettext-extractor/dist/utils/content";
 import { Validate } from "gettext-extractor/dist/utils/validate";
+import { DefaultTreeChildNode, Node, serialize } from "parse5";
+import treeAdapter from "parse5-htmlparser2-tree-adapter";
+
+type Template = Element & {
+  tagName: "template";
+  content: { nodeName: string; childNodes: DefaultTreeChildNode[] };
+};
+
+const getElementContent = (element: Element | Template, options: IContentOptions): string => {
+  let content = serialize(element, {});
+
+  // text nodes within template tags don't get serialized properly, this is a hack
+  if (element.tagName === "template") {
+    const docFragment = treeAdapter.createDocumentFragment();
+    (element as Template).content.childNodes.forEach((childNode: Node) => {
+      treeAdapter.appendChild(docFragment, childNode);
+    });
+    content = serialize(docFragment, {});
+  }
+
+  // Un-escape characters that get escaped by parse5
+  content = content.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+
+  return normalizeContent(content, options);
+};
 
 export function embeddedJsExtractor(selector: string, jsParser: JsParser): IHtmlExtractorFunction {
   Validate.required.nonEmptyString({ selector });
@@ -18,11 +43,12 @@ export function embeddedJsExtractor(selector: string, jsParser: JsParser): IHtml
     const element = node as Element;
 
     if (selectors.anyMatch(element)) {
-      const source = HtmlUtils.getElementContent(element, {
+      const source = getElementContent(element, {
         trimWhiteSpace: false,
         preserveIndentation: true,
         replaceNewLines: false,
       });
+
       jsParser.parseString(source, fileName, {
         scriptKind: 1,
         lineNumberStart: (element.sourceCodeLocation && element.sourceCodeLocation.startLine) || 0,
