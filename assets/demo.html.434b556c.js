62e88e6ae7d7b642f782a63de9ffdadda411e895
lzurbriggen <2656903+lzurbriggen@users.noreply.github.com>
2023-07-19 14:48:49 +0000

    Deploying to pages from @ jshmrtn/vue3-gettext@da825926bcf7863c76a24c0635bcd68dcf102551 🚀

---
+++ assets/demo.html.434b556c.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-2b30cdfd","path":"/demo.html","title":"Demo","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1689778081000,"contributors":[{"name":"Jacky","email":"jacky-943572677@qq.com","commits":1}]},"filePathRelative":"demo.md"}');export{e as data};
